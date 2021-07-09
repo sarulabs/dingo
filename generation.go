09cc9ce88e8a359a2d9fa730821c74526afd7e89
sarulabs <michael@sarulabs.com>
2021-07-09 23:00:39 +0200

    Allow package name customization

--- generation.go
+++ generation.go
@@ -11,12 +11,19 @@ import (
 // The definitions are loaded from the Provider.
 // The code is generated in the outputDirectory.
 func GenerateContainer(provider Provider, outputDirectory string) error {
+	return GenerateContainerWithCustomPkgName(provider, outputDirectory, "dic")
+}
+
+// GenerateContainerWithCustomPkgName works like GenerateContainer
+// but let you customize the package name in which files are generated.
+// The package name must be a valid package name or the generation may fail unexpectedly.
+func GenerateContainerWithCustomPkgName(provider Provider, outputDirectory, pkgName string) error {
 	scan, err := scanDefs(provider)
 	if err != nil {
 		return err
 	}
 
-	return writeScan(scan, outputDirectory)
+	return writeScan(scan, outputDirectory, pkgName)
 }
 
 func scanDefs(provider Provider) (*Scan, error) {
@@ -30,16 +37,17 @@ func scanDefs(provider Provider) (*Scan, error) {
 	return scan, nil
 }
 
-func writeScan(scan *Scan, outputDirectory string) error {
-	err := os.RemoveAll(outputDirectory + "/dic")
+func writeScan(scan *Scan, outputDirectory, pkgName string) error {
+	err := os.RemoveAll(outputDirectory + "/" + pkgName)
 	if err != nil {
 		return fmt.Errorf("could not remove destination directory: %v", err)
 	}
 
 	err = templates.WriteTemplate(
-		outputDirectory+"/dic/defs.go",
+		outputDirectory+"/"+pkgName+"/defs.go",
 		templates.DefsTemplate,
 		map[string]interface{}{
+			"PkgName": pkgName,
 			"Imports": scan.TypeManager.Imports(),
 			"Defs":    scan.Defs,
 		},
@@ -49,9 +57,10 @@ func writeScan(scan *Scan, outputDirectory string) error {
 	}
 
 	err = templates.WriteTemplate(
-		outputDirectory+"/dic/container.go",
+		outputDirectory+"/"+pkgName+"/container.go",
 		templates.ContainerTemplate,
 		map[string]interface{}{
+			"PkgName":         pkgName,
 			"Imports":         scan.ImportsWithoutParams,
 			"Defs":            scan.Defs,
 			"ProviderPackage": scan.ProviderPackage,
